3444bdb20ee270dd1495c93b84334bb670517157
T59_キジ_W <116822970+SatoshiKijima@users.noreply.github.com>
2023-01-06 12:16:58 +0900

    Add files via upload

---
+++ server_side_kadai3/delete.php
@@ -0,0 +1,27 @@
+<?php
+$id = $_GET['id'];
+
+// try {
+//     $db_name = 'gs_db3'; //データベース名
+//     $db_id   = 'root'; //アカウント名
+//     $db_pw   = ''; //パスワード：MAMPは'root'
+//     $db_host = 'localhost'; //DBホスト
+//     $pdo = new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host, $db_id, $db_pw);
+// } catch (PDOException $e) {
+//     exit('DB Connection Error:' . $e->getMessage());
+// }
+require_once('funcs.php');
+$pdo = db_conn();
+$stmt = $pdo->prepare('DELETE FROM gs_bm_table2 WHERE id = :id');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$status = $stmt->execute(); //実行
+
+if ($status === false) {
+    //*** function化する！******\
+    $error = $stmt->errorInfo();
+    exit('SQLError:' . print_r($error, true));
+} else {
+    //*** function化する！*****************
+    header('Location: select.php');
+    exit();
+}
---
+++ server_side_kadai3/select.php
@@ -0,0 +1,77 @@
+<?php
+//クロスサイトスクリプティングを防ぐ 
+//hが下が効いた状態で動く
+function h($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES);
+}
+//select.php 描写するときにhtmlspecialcharsで<script>タグで悪さされたものが文字列として入力される
+
+//1.  DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=kadai_php3;charset=utf8;host=localhost','root',''); 
+} catch (PDOException $e) {
+  exit('DBConnectError'.$e->getMessage());
+}
+
+//２．データ取得SQL作成
+$stmt = $pdo->prepare("SELECT * FROM gs_bm_table2;"); //あるデータベースから抜き出すからそのままでOK
+$status = $stmt->execute(); //実行するPHPのClassを理解しないと-＞は理解できない
+
+//３．データ表示
+//３．データ表示
+$view = '';
+if ($status === false) {
+    $error = $stmt->errorInfo();
+    exit('SQLError:' . print_r($error, true));
+} else {
+    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        //GETデータ送信リンク作成
+        // <a>で囲う。
+        $view .= '<p>';
+         $view .= '<a href="detail.php?id=' . $result['id'] . '">';
+        $view .= h($result['id']). '：' . h($result['time']) . '：' . h($result['bookname']). '：' . h($result['type']). '：';
+        $view .= '</a>';
+
+        $view .= '<a href="delete.php?id=' . $result['id'] . '">';
+        $view .= '[削除]';
+        $view .= '</a>';
+        $view .= '</p>';
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>書籍ログ表示</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">データ登録</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron"><?= $view ?></div> 
+   
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
